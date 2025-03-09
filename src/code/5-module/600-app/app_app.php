46308327f90187d282d98b0c1bb65e4d60c5afe3
John Elliot V <jj5@jj5.net>
2025-03-09 22:13:52 +1100

    Work, work...

--- src/code/5-module/600-app/app_app.php
+++ src/code/5-module/600-app/app_app.php
@@ -12,10 +12,10 @@ require_once __DIR__ . '/../../1-bootstrap/5-module.php';
 // 2025-03-09 jj5 - include components...
 //
 
-require_once __DIR__ . '/class/1-AppTask.php';
-require_once __DIR__ . '/class/2-AppShell.php';
-require_once __DIR__ . '/class/3-AppQuery.php';
-require_once __DIR__ . '/class/4-AppCommand.php';
-require_once __DIR__ . '/class/9-AppModule.php';
+require_once __DIR__ . '/1-class/1-AppTask.php';
+require_once __DIR__ . '/1-class/2-AppShell.php';
+require_once __DIR__ . '/1-class/3-AppQuery.php';
+require_once __DIR__ . '/1-class/4-AppCommand.php';
+require_once __DIR__ . '/1-class/9-AppModule.php';
 
 mud_load_deep_breadth_first( __DIR__ . '/2-shell' );
