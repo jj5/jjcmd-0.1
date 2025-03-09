831468613cc3ecb82157479ea2569a0b93fb839b
John Elliot V <jj5@jj5.net>
2025-03-09 22:51:05 +1100

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'JJCMD_NAME', 'Command-Line Tooling for Jay Jay' );
 define( 'JJCMD_CODE', 'jjcmd' );
 
-//define( 'JJCMD_VERSION', '0.1.11' );
+//define( 'JJCMD_VERSION', '0.1.13' );
 define( 'JJCMD_VERSION_MAJOR', 0 );
 define( 'JJCMD_VERSION_MINOR', 1 );
-define( 'JJCMD_VERSION_PATCH', 11 );
+define( 'JJCMD_VERSION_PATCH', 13 );
 
 define(
   'JJCMD_SVN_DATE',
-  '$Date: 2025-03-09 22:13:55 +1100 (Sun, 09 Mar 2025) $'
+  '$Date: 2025-03-09 22:51:05 +1100 (Sun, 09 Mar 2025) $'
 );
-define( 'JJCMD_SVN_REVISION', '$Revision: 73599026 $' );
+define( 'JJCMD_SVN_REVISION', '$Revision: 63203949 $' );
 define( 'JJCMD_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'JJCMD_GIT_DATE', '2025-03-09 22:13:55 +1100 (Sun, 09 Mar 2025)' );
+define( 'JJCMD_GIT_DATE', '2025-03-09 22:51:05 +1100 (Sun, 09 Mar 2025)' );
--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJCMD_NAME='Command-Line Tooling for Jay Jay';
 export JJCMD_CODE='jjcmd';
 
-export JJCMD_VERSION='0.1.11';
+export JJCMD_VERSION='0.1.13';
 export JJCMD_VERSION_MAJOR='0';
 export JJCMD_VERSION_MINOR='1';
-export JJCMD_VERSION_PATCH='11';
+export JJCMD_VERSION_PATCH='13';
 
-export JJCMD_SVN_DATE='Date: 2025-03-09 22:13:55 +1100 (Sun, 09 Mar 2025)';
-export JJCMD_SVN_REVISION='Revision: 73599026';
+export JJCMD_SVN_DATE='Date: 2025-03-09 22:51:05 +1100 (Sun, 09 Mar 2025)';
+export JJCMD_SVN_REVISION='Revision: 63203949';
 export JJCMD_SVN_AUTHOR='Author: jj5';
 
-export JJCMD_GIT_DATE='2025-03-09 22:13:55 +1100 (Sun, 09 Mar 2025)';
+export JJCMD_GIT_DATE='2025-03-09 22:51:05 +1100 (Sun, 09 Mar 2025)';
