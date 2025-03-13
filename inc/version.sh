43b22eb9301af086b24900ee3ec80fb899b9ce63
John Elliot V <jj5@jj5.net>
2025-03-13 22:55:56 +1100

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export JJCMD_NAME='Command-Line Tooling for Jay Jay';
 export JJCMD_CODE='jjcmd';
 
-export JJCMD_VERSION='0.1.37';
+export JJCMD_VERSION='0.1.39';
 export JJCMD_VERSION_MAJOR='0';
 export JJCMD_VERSION_MINOR='1';
-export JJCMD_VERSION_PATCH='37';
+export JJCMD_VERSION_PATCH='39';
 
-export JJCMD_SVN_DATE='Date: 2025-03-13 22:10:14 +1100 (Thu, 13 Mar 2025)';
-export JJCMD_SVN_REVISION='Revision: 179726439';
+export JJCMD_SVN_DATE='Date: 2025-03-13 22:55:56 +1100 (Thu, 13 Mar 2025)';
+export JJCMD_SVN_REVISION='Revision: 12515539';
 export JJCMD_SVN_AUTHOR='Author: jj5';
 
-export JJCMD_GIT_DATE='2025-03-13 22:10:14 +1100 (Thu, 13 Mar 2025)';
+export JJCMD_GIT_DATE='2025-03-13 22:55:56 +1100 (Thu, 13 Mar 2025)';
