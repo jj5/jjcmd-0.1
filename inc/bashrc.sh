1874346b76d55888e4184195bd849c23c15b4110
John Elliot V <jj5@jj5.net>
2025-03-17 01:52:19 +1100

    Work, work...

--- inc/bashrc.sh
+++ inc/bashrc.sh
@@ -4,12 +4,12 @@
 
 JJCMD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && realpath . )";
 JJCMD_DIR_BIN="$JJCMD_DIR/bin";
-JJCMD_DIR_ETC="$JJCMD_DIR/etc";
+#JJCMD_DIR_ETC="$JJCMD_DIR/etc";
 JJCMD_DIR_INC="$JJCMD_DIR/inc";
 JJCMD_DIR_SRC="$JJCMD_DIR/src";
 
 [ -d "$JJCMD_DIR_BIN" ] || { echo "error: JJCMD_DIR_BIN not found: $JJCMD_DIR_BIN"; exit 40; }
-[ -d "$JJCMD_DIR_ETC" ] || { echo "error: JJCMD_DIR_ETC not found: $JJCMD_DIR_ETC"; exit 40; }
+#[ -d "$JJCMD_DIR_ETC" ] || { echo "error: JJCMD_DIR_ETC not found: $JJCMD_DIR_ETC"; exit 40; }
 [ -d "$JJCMD_DIR_INC" ] || { echo "error: JJCMD_DIR_INC not found: $JJCMD_DIR_INC"; exit 40; }
 [ -d "$JJCMD_DIR_SRC" ] || { echo "error: JJCMD_DIR_SRC not found: $JJCMD_DIR_SRC"; exit 40; }
 
