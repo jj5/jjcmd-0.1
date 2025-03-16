1fdac973a895d3ce3c1679f0c5951e772ffa5877
John Elliot V <jj5@jj5.net>
2025-03-17 00:41:40 +1100

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'JJCMD_NAME', 'Command-Line Tooling for Jay Jay' );
 define( 'JJCMD_CODE', 'jjcmd' );
 
-//define( 'JJCMD_VERSION', '0.1.87' );
+//define( 'JJCMD_VERSION', '0.1.89' );
 define( 'JJCMD_VERSION_MAJOR', 0 );
 define( 'JJCMD_VERSION_MINOR', 1 );
-define( 'JJCMD_VERSION_PATCH', 87 );
+define( 'JJCMD_VERSION_PATCH', 89 );
 
 define(
   'JJCMD_SVN_DATE',
-  '$Date: 2025-03-17 00:30:33 +1100 (Mon, 17 Mar 2025) $'
+  '$Date: 2025-03-17 00:41:39 +1100 (Mon, 17 Mar 2025) $'
 );
-define( 'JJCMD_SVN_REVISION', '$Revision: 6043361 $' );
+define( 'JJCMD_SVN_REVISION', '$Revision: 217572211 $' );
 define( 'JJCMD_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'JJCMD_GIT_DATE', '2025-03-17 00:30:33 +1100 (Mon, 17 Mar 2025)' );
+define( 'JJCMD_GIT_DATE', '2025-03-17 00:41:39 +1100 (Mon, 17 Mar 2025)' );
